fe15dc1d9adc398f4a555ca04be8e95a051910d8
Javier Saldana <javier@tractical.com>
2013-04-26 18:31:23 -0500

    update tpb endpoint, closes #1

--- lib/the_pirate_bay.rb
+++ lib/the_pirate_bay.rb
@@ -6,27 +6,27 @@ require "the_pirate_bay/model"
 require "the_pirate_bay/client"
 
 module ThePirateBay
-  
-  ENDPOINT = "http://thepiratebay.se".freeze
-  
+
+  ENDPOINT = "http://thepiratebay.is".freeze
+
   class << self
-        
+
     # Handle for the client instance
     attr_accessor :api_client
-  
+
     def new(options={}, &block)
       @api_client = ThePirateBay::Client.new(options, &block)
     end
-    
+
     # Delegate to ThePirateBay::Client
     def method_missing(method, *args, &block)
       return super unless new.respond_to?(method)
       new.send(method, *args, &block)
     end
-    
+
     def respond_to?(method, include_private = false)
       new.respond_to?(method, include_private) || super(method, include_private)
     end
-    
+
   end # Self
 end # ThePirateBay
